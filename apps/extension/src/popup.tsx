49d9e61ee4de393d12d0553220e3b4bfcbb0d9aa
Richard Poelderl <richard.poelderl@gmail.com>
2022-10-05 18:06:34 +0200

    setup tailwind

--- apps/extension/src/popup.tsx
+++ apps/extension/src/popup.tsx
@@ -2,26 +2,12 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 import App from "./app"
 
-export const queryClient = new QueryClient()
+import "./style.css"
 
 function IndexPopup() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          padding: 16,
-          width: 780,
-          height: 600
-        }}>
-        <h1>Is this big enough for our calendar?</h1>
-        <p>This would even work in a new tab</p>
-        <p>
-          Have seen this quite a bit when people wanted to open a new tab and
-          use our ext
-        </p>
-      </div>
+      <div className="w-[800] h-[600]">
       <App />
     </QueryClientProvider>
   )
