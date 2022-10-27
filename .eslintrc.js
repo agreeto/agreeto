1234744357d90e65c495ed535ad18493e872fc56
Richard Poelderl <richard.poelderl@gmail.com>
2022-10-27 14:19:13 +0200

    try following turborepos guide

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,11 @@
-module.exports = require("./packages/config/eslint-preset");
+module.exports = {
+  root: true,
+  // This tells ESLint to load the config from the package `eslint-config-agreeto`
+  // https://turbo.build/repo/docs/getting-started/create-new#understanding-eslint-config-custom
+  extends: ["agreeto"],
+  settings: {
+    next: {
+      rootDir: ["apps/*/"],
+    },
+  },
+};
