dfbf5cc85a505690f2d5e0731e74aa503213ca2f
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-10-29 10:55:03 +0200

    add optional flag to enable next-config to be loaded without valid env

--- packages/config/eslint/index.js
+++ packages/config/eslint/index.js
@@ -1,16 +1,18 @@
 module.exports = {
-  extends: [
-    "next",
-    "turbo",
-    "eslint:recommended",
-    "@typescript-eslint/recommended",
-    "prettier",
-  ],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
     project: "./tsconfig.json",
   },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "next",
+    "turbo",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
