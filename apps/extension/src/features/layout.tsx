b7f958a2413867d31cef84cbfd6e0e12cd4031f0
Richard Poelderl <richard.poelderl@gmail.com>
2022-10-31 15:28:12 +0100

    feat(extension): show `Add Account` & `Sign Out` button only when authenticated

--- apps/extension/src/features/layout.tsx
+++ apps/extension/src/features/layout.tsx
@@ -2,9 +2,19 @@ import { MinusCircleIcon } from "@heroicons/react/20/solid"
 import * as SeparatorPrimitive from "@radix-ui/react-separator"
 import type { FC, ReactNode } from "react"
 
+import { useStorage } from "@plasmohq/storage/hook"
+
 import { Navbar } from "~features/navbar"
 
+import { AccessToken } from "./auth"
+
 const Layout: FC<{ children?: ReactNode }> = ({ children }) => {
+  const [accessTokenValue] = useStorage({
+    key: "accessToken",
+    isSecret: true
+  })
+  const authentication = AccessToken.safeParse(accessTokenValue)
+
   return (
     <div className="w-full divide-y">
       {/* WINDOW MANAGER */}
@@ -15,39 +25,45 @@ const Layout: FC<{ children?: ReactNode }> = ({ children }) => {
           className="inline-flex items-center p-1 text-white bg-indigo-600 border border-transparent rounded-full shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
           <MinusCircleIcon className="w-5 h-5" aria-hidden="true" />
         </button>
-        <SeparatorPrimitive.Root
-          decorative
-          orientation="vertical"
-          className="w-px mx-4 bg-indigo-600 h-inherit"
-          data-aria-orientation="vertical"
-        />
-        <button
-          type="button"
-          className="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-          onClick={() => {
-            window.open(
-              `http://localhost:3000/api/auth/signin?${new URLSearchParams({
-                callbackUrl: `${process.env
-                  .PLASMO_PUBLIC_WEB_URL!}/auth/extension`
-              })}`
-            )
-          }}>
-          Add Account
-        </button>
-        <SeparatorPrimitive.Root
-          decorative
-          orientation="vertical"
-          className="w-px mx-4 bg-indigo-600 h-inherit"
-          data-aria-orientation="vertical"
-        />
-        <button
-          type="button"
-          className="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-          onClick={() => {
-            window.open(`${process.env.PLASMO_PUBLIC_WEB_URL!}/auth/signout`)
-          }}>
-          Sign Out
-        </button>
+        {authentication.success && (
+          <>
+            <SeparatorPrimitive.Root
+              decorative
+              orientation="vertical"
+              className="w-px mx-4 bg-indigo-600 h-inherit"
+              data-aria-orientation="vertical"
+            />
+            <button
+              type="button"
+              className="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              onClick={() => {
+                window.open(
+                  `http://localhost:3000/api/auth/signin?${new URLSearchParams({
+                    callbackUrl: `${process.env
+                      .PLASMO_PUBLIC_WEB_URL!}/auth/extension`
+                  })}`
+                )
+              }}>
+              Add Account
+            </button>
+            <SeparatorPrimitive.Root
+              decorative
+              orientation="vertical"
+              className="w-px mx-4 bg-indigo-600 h-inherit"
+              data-aria-orientation="vertical"
+            />
+            <button
+              type="button"
+              className="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              onClick={() => {
+                window.open(
+                  `${process.env.PLASMO_PUBLIC_WEB_URL!}/auth/signout`
+                )
+              }}>
+              Sign Out
+            </button>
+          </>
+        )}
       </div>
       {/* ACTUAL APP */}
       <div className="flex h-full">
