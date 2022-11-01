3dee23946bfa49df916408f96dcdb92df62fd927
Richard Poelderl <richard.poelderl@gmail.com>
2022-11-01 12:33:38 +0100

    chore(extension): commented our unused code? (was mainly for my understanding)

--- apps/extension/src/features/router/config.tsx
+++ apps/extension/src/features/router/config.tsx
@@ -1,14 +1,13 @@
 import {
-  type ReactLocationOptions,
-  type Route,
+  type ReactLocationOptions, // type Route,
   createMemoryHistory
 } from "@tanstack/react-location"
 
-import { SignIn } from "~features/auth"
-import { trpcApi } from "~features/trpc/api/hooks"
+// import { SignIn } from "~features/auth"
+// import { trpcApi } from "~features/trpc/api/hooks"
 
-import { Calendar } from "./routes/calendar"
-import { Settings } from "./routes/settings"
+// import { Calendar } from "./routes/calendar"
+// import { Settings } from "./routes/settings"
 
 // Create a memory history
 export const reactLocationOptions: ReactLocationOptions = {
@@ -17,35 +16,36 @@ export const reactLocationOptions: ReactLocationOptions = {
   })
 }
 
-export const getRoutes: () => Route[] = () => {
-  const utils = trpcApi.useContext()
-  return [
-    {
-      path: "/",
-      element: <SignIn />,
-      children: [
-        {
-          path: "calendar",
-          // loader: async () => {
-          //   const user = await utils.user.current.fetch()
-          //   const events = await utils.post.all.fetch()
-          //   return { user, events }
-          // },
-          element: <Calendar />
-        },
-        {
-          path: "settings",
-          element: <Settings />,
-          // TODO: add account fetching to the settings route
-          async loader({ params }) {
-            console.log({ params })
-            // FIXME: Should prob not include all this information
-            utils.user.myAccounts.fetch()
-            return {}
-            // accounts: await utils.account.all.fetch()
-          }
-        }
-      ]
-    }
-  ]
-}
+// REVIEW (richard): this is not used anymore?
+// export const getRoutes: () => Route[] = () => {
+//   const utils = trpcApi.useContext()
+//   return [
+//     {
+//       path: "/",
+//       element: <SignIn />,
+//       children: [
+//         {
+//           path: "calendar",
+//           // loader: async () => {
+//           //   const user = await utils.user.current.fetch()
+//           //   const events = await utils.post.all.fetch()
+//           //   return { user, events }
+//           // },
+//           element: <Calendar />
+//         },
+//         {
+//           path: "settings",
+//           element: <Settings />,
+//           // TODO: add account fetching to the settings route
+//           async loader({ params }) {
+//             console.log({ params })
+//             // FIXME: Should prob not include all this information
+//             utils.user.myAccounts.fetch()
+//             return {}
+//             // accounts: await utils.account.all.fetch()
+//           }
+//         }
+//       ]
+//     }
+//   ]
+// }
