aa8f5989d718ab3318a366419ce1cc3308fbce83
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-11-07 12:55:30 +0100

    fix linter config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,9 +3,4 @@ module.exports = {
   // This tells ESLint to load the config from the package `eslint-config-agreeto`
   // https://turbo.build/repo/docs/getting-started/create-new#understanding-eslint-config-custom
   extends: ["agreeto"],
-  settings: {
-    next: {
-      rootDir: ["apps/*/"],
-    },
-  },
 };
