50302dab38bcc5f277407fa28106f3a8a4526bc2
Richard Poelderl <richard.poelderl@gmail.com>
2022-11-11 19:37:10 +0100

    add agreeto/ui package to nextjs and render button

--- packages/ui/src/button/button.tsx
+++ packages/ui/src/button/button.tsx
@@ -1,6 +1,9 @@
+import type { ReactNode } from "react";
+import React from "react";
+
 export const Button: React.FC<{
   onClick?: () => void;
-  title: string;
+  children: ReactNode;
 }> = (props) => {
   return (
     <button
@@ -8,7 +11,7 @@ export const Button: React.FC<{
       className="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
       onClick={props.onClick}
     >
-      {props.title}
+      {props.children}
     </button>
   );
 };
