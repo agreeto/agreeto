49c0c2cded7001d638f93bc043c08265c6ab4cfc
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-11-12 14:45:40 +0100

    fix jump to definition

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from "tsup";
+import { execSync } from "child_process";
 
-export default defineConfig({
+// `opts` are brought in from CLI args
+export default defineConfig((opts) => ({
   clean: true,
   entry: ["src/index.tsx"],
   format: ["esm", "cjs"],
   dts: true,
   external: ["react"],
   sourcemap: true,
-  minify: process.env.NODE_ENV === "production",
+  minify: !opts.watch,
   outDir: "dist",
-});
+  async onSuccess() {
+    // emit sourcemap to enable jump to definition
+    execSync("pnpm tsc --project tsconfig.sourcemap.json");
+  },
+}));
