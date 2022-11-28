1a2320e08a93f771625f1f7f63a91898eadeee56
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-11-28 23:09:35 +0100

    format

--- apps/extension/tailwind.config.js
+++ apps/extension/tailwind.config.js
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   presets: [require("@agreeto/tailwind-config")],
-  content: ["./src/**/*.{ts,tsx}", "../../packages/ui/src/**/*.{tsx,ts}", "../../packages/calendar/react/src/**/*.{tsx,ts}"],
+  content: [
+    "./src/**/*.{ts,tsx}",
+    "../../packages/ui/src/**/*.{tsx,ts}",
+    "../../packages/calendar/react/src/**/*.{tsx,ts}",
+  ],
 };
