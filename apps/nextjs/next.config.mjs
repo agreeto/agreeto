66b873d2fc1b8f67e666732ae1ad477fe4985975
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-11-28 17:25:33 +0100

    env stuff

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -4,13 +4,19 @@ import withTM from "next-transpile-modules";
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. Usage: `SKIP_ENV_VALIDATION=1 pnpm lint`.
  * This is especially useful for Docker builds or linting.
+ * We also skip validating in CI since that's not necessary for now.
  */
-!process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
+!process.env.SKIP_ENV_VALIDATION &&
+  !process.env.CI &&
+  (await import("./src/env/server.mjs"));
 
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
   swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: !!process.env.CI,
+  },
 };
 
 export default withTM(["@agreeto/api", "@agreeto/auth", "@agreeto/db"])(config);
