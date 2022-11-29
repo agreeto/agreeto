15e275c8be6b2bf154794bd4dae3556e576ce18a
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-11-29 01:34:09 +0100

    some clsx

--- packages/calendar/react/src/components/calendar-item/calendar-item.tsx
+++ packages/calendar/react/src/components/calendar-item/calendar-item.tsx
@@ -32,6 +32,7 @@ import TimeZoneSelect from "./time-zone-select";
 import { trpc } from "../../utils/trpc";
 import { useCalendarStore, useEventStore, useTZStore } from "../../utils/store";
 import { themeColors } from "../../utils/colors";
+import clsx from "clsx";
 
 type Props = {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -108,9 +109,10 @@ const CalendarItem: FC<Props> = ({ onRefSettled, onPageChange }) => {
       <div>
         <div className="font-normal text-gray-600">{day}</div>
         <div
-          className={`my-1 flex h-10 w-10 items-center justify-center rounded-full text-xl font-semibold ${
-            isToday ? "bg-primary text-white" : "text-gray-600"
-          }`}
+          className={clsx(
+            "my-1 flex h-10 w-10 items-center justify-center rounded-full text-xl font-semibold",
+            isToday ? "bg-primary text-white" : "text-gray-600",
+          )}
         >
           {date.getDate()}
         </div>
@@ -129,9 +131,12 @@ const CalendarItem: FC<Props> = ({ onRefSettled, onPageChange }) => {
 
     return (
       <div
-        className={`h-full overflow-hidden ${
-          extendedProps?.isDeclined ? "line-through" : ""
-        } ${shrinkFont ? "" : "p-1"} ${diff <= 15 ? "flex space-x-1" : ""} `}
+        className={clsx(
+          "h-full overflow-hidden",
+          extendedProps?.isDeclined && "line-through",
+          !shrinkFont && "p-1",
+          diff <= 15 && "flex space-x-1",
+        )}
         style={{
           // color: event.textColor,
           lineHeight: shrinkFont ? 1 : undefined,
@@ -141,7 +146,9 @@ const CalendarItem: FC<Props> = ({ onRefSettled, onPageChange }) => {
 ${extractEventHours(event)}`} // This is not a lint error. The space is left here on purpose for line breaks in the title
       >
         {/* Title */}
-        <div className={`${shrinkFont ? "text-2xs" : "text-xs"} font-semibold`}>
+        <div
+          className={clsx("font-semibold", shrinkFont ? "text-2xs" : "text-xs")}
+        >
           {event.title}
         </div>
         {/* Event hours */}
@@ -149,13 +156,12 @@ ${extractEventHours(event)}`} // This is not a lint error. The space is left her
         {/* Cancel button */}
         {extendedProps?.new && (
           <div
-            className={
-              "absolute flex h-4 w-4 cursor-pointer items-center justify-center rounded-full bg-primary text-white"
-            }
-            style={{
-              top: extendedProps?.new ? "-10px" : "-8px",
-              right: extendedProps?.new ? "-8px" : "-6px",
-            }}
+            className={clsx(
+              "absolute flex h-4 w-4 cursor-pointer items-center justify-center rounded-full bg-primary text-white",
+              extendedProps?.new
+                ? "top-[-10px] right-[-8px]"
+                : "top-[-8px] right-[-6px]",
+            )}
             onClick={() => {
               deleteSlot(event.toJSON());
             }}
@@ -170,11 +176,12 @@ ${extractEventHours(event)}`} // This is not a lint error. The space is left her
   const renderSlotLabels = ({ time }: SlotLabelContentArg) => {
     return (
       <div
-        className={`text-ceter flex text-xs text-gray-300 ${
+        className={clsx(
+          "text-ceter flex text-xs text-gray-300",
           timeZones.length === 1
             ? "w-14 justify-center"
-            : "w-24 justify-between pr-2"
-        }`}
+            : "w-24 justify-between pr-2",
+        )}
         style={{
           lineHeight: "12px", // This is for slot height. Update this and fc-timegrid-slot in calendar-item.scss to chage the height
         }}
@@ -374,12 +381,10 @@ ${extractEventHours(event)}`} // This is not a lint error. The space is left her
 
       {/* Timezone title */}
       <div
-        className="absolute top-7 flex justify-around"
-        style={{
-          width: timeZones.length === 1 ? "64px" : "120px",
-          left: timeZones.length === 1 ? "-14px" : "-8px",
-          paddingRight: timeZones.length === 1 ? "0" : "8px",
-        }}
+        className={clsx(
+          "absolute top-7 flex justify-around",
+          timeZones.length === 1 ? "-left-4 w-16" : "-left-2 w-32 pr-2",
+        )}
       >
         {/* Plus icon */}
         {timeZones.length === 1 && (
